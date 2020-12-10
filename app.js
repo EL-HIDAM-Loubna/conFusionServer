81718039e73d33ba842cc11bcec5521c7874bb56
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-12-10 16:40:53 +0100

    Cookies

--- app.js
+++ app.js
@@ -13,39 +13,57 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
+
 connect.then((db) => {
   console.log('connected correctly to server');
 }, (err) => { console.log(err); });
 
+
 var app = express();
+app.use(cookieParser('12345-67890-09876-54321'));
+
+
 function auth(req, res, next){
-  console.log(req.headers);
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status = 401;
+        next(err);
+    }
 
-  var authHeader = req.headers.authorization;
-  if(! authHeader){
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    next(err);
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+    var username = auth[0];
+    var password = auth[1];
+
+    if(username === 'admin' && password === 'password'){
+        res.cookie('user','admin', {signed: true});
+        next();
+    }
+    else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
+    }
   }
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
 
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username === 'admin' && password === 'password'){
-    next();
-  }
-  else {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
+  else{
+    if(req.signedCookies.user === 'admin'){
+        next();
+    }
+    else{
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
   }
 }
 
 app.use(auth);
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -54,7 +72,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
