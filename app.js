a4de6215b8f766a81ea3f2747950cea7fa0b9e42
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-12-14 19:06:27 +0100

    Assignment 3: User Authentication

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ connect.then((db) => {
 
 
 var app = express();
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -10,6 +10,14 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
+const verifySameUser = (authorId, commentAuthorId, next) => {
+    if (!authorId.equals(commentAuthorId)) {
+      const err = new Error('You are not the author of the comment')
+      err.code = 403;
+      next(err)
+    }
+  }
+
 dishRouter.route('/')
 .get((req, res, next) => {
     Dishes.find({}).populate('comments.author').then((dishes) => {
@@ -19,7 +27,7 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.create(req.body).then((dish) => {
         console.log('Dish created ', dish);
         res.statusCode = 200;
@@ -28,11 +36,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(authenticate.verifyUser,(req, res, next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not suported on /dishes');
 })
-.delete(authenticate.verifyUser,(req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
    Dishes.remove({}).then((resp) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -50,11 +58,11 @@ dishRouter.route('/:dishId')
 }, (err) => next(err))
 .catch((err) => next(err));
 })
-.post(authenticate.verifyUser,(req, res, next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
   res.statusCode = 403;
   res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put(authenticate.verifyUser,(req, res, next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
   Dishes.findByIdAndUpdate(req.params.dishId, {$set: req.body}, {new: true})
   .then((dish) => {
     res.statusCode = 200;
@@ -63,7 +71,7 @@ dishRouter.route('/:dishId')
 }, (err) => next(err))
 .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser,(req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp) => {
         res.statusCode = 200;
@@ -119,7 +127,7 @@ dishRouter.route('/:dishId/comments')
     res.statusCode = 403;
     res.end('PUT operation not suported on /dishes/'+req.params.dishId+'/comments');
 })
-.delete(authenticate.verifyUser,(req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin, (req, res, next) => {
    Dishes.findById(req.params.dishId).then((dish) => {
        if(dish != null){
            for(var i = (dish.comments.length -1); i >= 0; i--){
@@ -170,7 +178,8 @@ dishRouter.route('/:dishId/comments/:commentId')
 .put(authenticate.verifyUser,(req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
-     if(dish != null && dish.comments.id(req.params.commentId) != null){
+     if(dish != null && dish.comments.id(req.params.commentId) != null){ 
+        verifySameUser(req.user._id, dish.comments.id(req.params.commentId).author, next);
         if(req.body.rating){
             dish.comments.id(req.params.commentId).rating = req.body.rating;
         }
@@ -186,7 +195,7 @@ dishRouter.route('/:dishId/comments/:commentId')
                     res.json(dish);  
                 })  
         }, (err) => next(err));
-        
+    
      }
      else if(dish == null){
          err = new Error('Dish '+ req.params.dishId + ' Not Found!');
@@ -204,6 +213,7 @@ dishRouter.route('/:dishId/comments/:commentId')
 .delete(authenticate.verifyUser,(req, res, next) => {
     Dishes.findById(req.params.dishId).then((dish) => {
         if(dish != null && dish.comments.id(req.params.commentId) != null){
+            verifySameUser(req.user._id, dish.comments.id(req.params.commentId).author);
             dish.comments.id(req.params.commentId).remove();
             dish.save().then((dish) => {
                 Dishes.findById(dish._id)
