665432dd6161e3a55adb2370790ba9933200d7ea
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-12-15 11:32:50 +0100

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId:'221197299465688',
+        clientSecret:'c1878d68dcb156826629a1ef4134b781'
+    }
 }
